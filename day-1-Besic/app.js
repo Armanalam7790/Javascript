c48b4de909a0aef5db25b42022f37140e8ab3d51
Armanalam7790 <alamarman7790@gmail.com>
2026-02-06 12:20:24 +0530

    day1

---
+++ day-1-Besic/app.js
@@ -0,0 +1,43 @@
+const user  = "Arman"
+const age  = 30
+
+console.log(`user name is ${user} and age is ${age}`)
+
+alert("Messages")
+
+
+
+ var ans  = confirm("are you man")
+ console.log(ans);
+ 
+
+  const  el  = prompt("Enter anything..")
+  console.log(el)
+
+
+  //elert  = for message
+  //confirm  = yes or no?
+  //prompt = write anything...
+
+  //console staement
+  //variable declare and init
+  //alert confirm prompt
+
+  var a = 20;
+  var b = 7;
+  console.log(a/b)
+
+//   datatype in js
+
+//   premitiv
+//      - Number  (10,23,-34)
+//      - Boolean (true,false)
+//      - String  ('a' , "arman")
+//      - undefined  without initialized that called undifind
+//      - null (nothing)
+//      - NaN  not a number
+
+//      reference
+//        - Array  
+//        - Object
+//        - Function
