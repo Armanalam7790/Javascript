6ba24d5dbef3d4c99083982e875765b49657a917
Armanalam7790 <alamarman7790@gmail.com>
2026-02-07 10:21:19 +0530

    loops if else-if

---
+++ day-2/style.js
@@ -0,0 +1,130 @@
+
+
+// if (10>5) {
+//     console.log("hello");
+    
+// }
+
+
+// var a  = 10;
+// var b  = 30;
+
+// if (a>b) {
+//     console.log("hello world") 
+// }
+// else{
+//     console.log("byyyyyyy");
+    
+// }
+
+
+
+// var age  = Number(prompt("Enter Age"))
+
+
+
+
+// if (isNaN(age)) {
+//    console.log("enter correct age");
+// }
+// else if(age>= 18){
+//     console.log('you can vote');
+    
+// }
+// else{
+//    console.log('you can not vote')
+    
+// }
+
+
+//ask user his salary and if is more 50000 then bonus will be of 10% and if less then bonus be of 20% now print final salary
+
+// var salary =  +prompt("enter your monthly sallery")
+
+// var finalsallery   = 0 ;
+
+// if (salary>=50000) {
+//     finalsallery = salary + (salary*10)/100
+
+    
+// }else{
+//      console.log("20% bonus");
+//       finalsallery = salary + (salary*20)/100
+// }
+// console.log(finalsallery);
+
+
+// var marks  = 65;
+// if (marks>90) {
+//     console.log('Topper')
+// }
+// else if(marks>=80){
+//     console.log('good progress');
+    
+// }
+// else if(marks>=70){
+//     console.log('good prog');
+    
+// }
+// else if(marks>=60){
+//     console.log('good pro');
+    
+// }
+// else if(marks>=50){
+//     console.log('good ');
+    
+// }
+
+// else{
+//     console.log('fail')
+// }
+
+
+// var age = +prompt("enter your age")
+// console.log(age);
+// if (age>=18 && age<=60) {
+//     console.log('you can go oyo .....');
+    
+// }else{
+//     console.log('chala ja bsdk .....');
+
+// }
+
+//   || (OR operator)
+
+//while loop
+
+// var a = 0
+// while (a<10) {
+//     console.log(a);
+//     a++
+// }
+
+
+// ask user any natural number and print all number till that number
+//  let num = +prompt("number")
+// var a = 1;
+// while (a<= num) {
+//     console.log(a);
+//     a++
+    
+// }
+
+
+// var a = 40;
+// do {
+//     console.log("hello");
+//     a++
+    
+// } while (a<=5);
+
+
+// do {
+//   var user  =   prompt("enter name")
+// } while (user != "admin" );
+// console.log("welcome");
+
+// for (let i = 0; i <  10; i++) {
+//     console.log(i);
+    
+// }
