d900e936bf0b3f9239f15ee21639aea1f272a0b9
Armanalam7790 <alamarman7790@gmail.com>
2026-02-25 10:03:56 +0530

    added day-28 with 3 projects

---
+++ day-28/task1/index.js
@@ -0,0 +1,17 @@
+
+
+var nav = document.querySelector('nav')
+
+ var body = document.body
+ body.addEventListener('wheel' , function(dets) {
+   if (dets.deltaY>0) {
+    console.log('scrooling');
+    nav.style.transform= 'translateY(-100%)'
+    
+   }
+   else {
+    nav.style.transform= 'translateY(0%)'
+   }
+   
+    
+ })
