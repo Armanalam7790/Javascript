05e3e9976b3b956c842a5065faafb6aff94b1a42
Armanalam7790 <alamarman7790@gmail.com>
2026-04-01 23:37:19 +0530

    prectice

---
+++ question-prectice/index.js
@@ -0,0 +1,56 @@
+
+
+
+// let num = +prompt('Enter marks:');
+
+// if (num >= 90 && num <= 100) {
+//     console.log('A');
+// } else if (num >= 80 && num < 90) {
+//     console.log('B');
+// } else if (num >= 70 && num < 80) {
+//     console.log('C');
+// } else if (num >= 60 && num < 70) {
+//     console.log('D');
+// } else if (num >= 0 && num < 60) {
+//     console.log('F');
+// } else {
+//     console.log('Invalid marks');
+// }
+
+// // Even/Odd — Number even hai ya odd batao.
+
+// let num =  +prompt('Number')
+
+// if (num%2===0) {
+//     console.log('EVEN NUMBER', num);
+    
+    
+// } else{
+//     console.log('ODD NUMBER', num);
+
+// }
+
+// Largest of Three — Teen numbers mein se sabse bada find karo (without Math.max).
+
+
+// let a = 14, b = 17, c = 15;
+
+// if (
+//   a >= 13 && a <= 19 &&
+//   b >= 13 && b <= 19 &&
+//   c >= 13 && c <= 19
+// ) {
+//   let max = a;
+
+//   if (b > max) {
+//     max = b;
+//   }
+
+//   if (c > max) {
+//     max = c;
+//   }
+
+//   console.log("Largest:", max);
+// } else {
+//   console.log("All numbers must be between 13 and 19");
+// }
